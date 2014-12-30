47873a6d3d5704bb5772fc7d41d4e9d591a17dc4
Jake <jake@jakebladt.com>
2014-12-30 14:42:42 -0500

    +birthday

---
+++ sandbox/c/004ptr01/app.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+  char age_c[8];
+  int age;
+
+  puts("Enter your age: ");
+  scanf("%7s", age_c);
+  age = atoi(age_c);
+  puts("You're %i.", &age);
+
+  puts("You have a birthday.");
+  age_up(age);
+  puts("Now you're %i.", age);
+
+  return 0;
+}
+
+void age_up(int *age)
+{
+  *age++;
+}
