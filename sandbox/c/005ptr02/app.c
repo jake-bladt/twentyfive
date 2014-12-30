ea774c843faeb386a83f460a5fd2d134c7fcb3b6
Jake <jake@jakebladt.com>
2014-12-30 15:17:33 -0500

    salaries

--- sandbox/c/005ptr02/app.c
+++ sandbox/c/005ptr02/app.c
@@ -4,6 +4,10 @@
 int main()
 {
   int salary_levels[] = { 70000, 80000, 95000, 110000 };
-  printf("The highest salary level is %i.\n", 3[salary_levels]);
+  int i;
+  for(i = 0; i < 4; i++)
+  {
+    printf("Salary for engineer level %i is %i.\n", i, 3[salary_levels]);
+  }
   return 0;
 }
