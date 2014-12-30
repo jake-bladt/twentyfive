f32beb02f61817127eacd392b59b68da44ef2df6
Jake <jake@jakebladt.com>
2014-12-30 15:35:21 -0500

    stay cool

---
+++ sandbox/c/006ptr03/app.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+const char *warning_message = "Warning!";
+
+int main()
+{
+  char[] stay_cool = warning_message;
+  stay_cool[0] = 'M';
+  stay_cool[1] = 'o';
+  printf(stay_cool);
+}
