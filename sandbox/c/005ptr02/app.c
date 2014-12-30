923234afb1d87fdbe6a821da153b81ce962341ac
Jake <jake@jakebladt.com>
2014-12-30 15:15:49 -0500

    +newline

--- sandbox/c/005ptr02/app.c
+++ sandbox/c/005ptr02/app.c
@@ -4,6 +4,6 @@
 int main()
 {
   int salary_levels[] = { 70000, 80000, 95000, 110000 };
-  printf("The highest salary level is %i.", 3[salary_levels]);
+  printf("The highest salary level is %i.\n", 3[salary_levels]);
   return 0;
 }
