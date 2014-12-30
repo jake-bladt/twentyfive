d7c1940acf7767fef4211bdaa5e8371b0f3617dd
Jake <jake@jakebladt.com>
2014-12-30 13:28:55 -0500

    build

--- sandbox/c/002scanf/app.c
+++ sandbox/c/002scanf/app.c
@@ -5,6 +5,5 @@ int main()
   char your_name[64];
   puts("Enter your name: ");
   scanf("%63s", your_name);
-  printf("Welcome to twenty-five, %s\n", your_name);
+  printf("Welcome to twenty-five, %s!\n", your_name);
 }
-
