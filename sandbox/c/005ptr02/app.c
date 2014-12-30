e8fc02d3d6b0ff263b6a325e09e3a1731498b0fc
Jake <jake@jakebladt.com>
2014-12-30 15:13:12 -0500

    move array

--- sandbox/c/005ptr02/app.c
+++ sandbox/c/005ptr02/app.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-  int[] salary_levels = { 70000, 80000, 95000, 110000 };
+  int salary_levels[] = { 70000, 80000, 95000, 110000 };
   printf("The highest salary level is %i.", 3[salary_levels]);
   return 0;
 }
