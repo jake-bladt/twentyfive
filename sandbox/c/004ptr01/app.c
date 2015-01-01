1364f04fd28bf2ee802b3ba0493b75d82e340958
Jake Bladt <jake@jakebladt.com>
2015-01-01 18:19:12 -0500

    directly accept integer

--- sandbox/c/004ptr01/app.c
+++ sandbox/c/004ptr01/app.c
@@ -9,12 +9,10 @@ void age_up(int* age)
 
 int main() 
 {
-  char age_c[8];
   int age;
 
   printf("Enter your age: ");
-  scanf("%7s", age_c);
-  age = atoi(age_c);
+  scanf("%i", &age);
   printf("You're %i.\n", age);
 
   printf("You have a birthday.\n");
