38c26a077bdc6deb7b21835e3fd2bcbdb42a34bd
Jake Bladt <jake@jakebladt.com>
2015-01-03 22:43:25 -0500

    secretword

--- sandbox/c/008string02/app.c
+++ sandbox/c/008string02/app.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
+char[][16] secret_words = { "yes", "no", "the", "lotion", "admin", "fnord", "secret" };
 
+int test_for_secret_words(char[] input)
+{
+  int i;
+  for(i = 0; i < 7, i++) {
+    if(strstr(input, secret_words[i]) {
+      return i;
+    }
+  }
+  return -1;
+}
 
 int main()
 {
-
-	return 0;
+  char input_sentence[80];
+  printf("Search for: ");
+  scanf("%79s", search_for);
+  int res;
+  res = test_for_secret_words(search_for);
+  if(res == -1) {
+    puts("No secret words found.");
+  } else {
+    printf("Found secret word %s", secret_words[res]);
+  }
+  return 0;
 }
