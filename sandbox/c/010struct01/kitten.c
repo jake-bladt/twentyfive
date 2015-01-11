3730870cb3214e4ea3e68a941f581fbb9092399e
Jake Bladt <jake@jakebladt.com>
2015-01-10 22:02:59 -0500

    kitten->k

--- sandbox/c/010struct01/kitten.c
+++ sandbox/c/010struct01/kitten.c
@@ -3,12 +3,13 @@
 
 void get_age(struct kitten *k, char *age_msg)
 {
-	sprintf(age_msg, "%i", k->age);
+	int numeric_age = (k->age.overflow == "many" ? 4 : k->age.years);
+	sprintf(age_msg, "%i", numeric_age);
 }
 
 void purr(struct kitten *k, char *msg)
 {
 	char age_msg[8];
-	get_age(kitten, age_msg);
+	get_age(k, age_msg);
 	sprintf(msg, "Purr. My name is %s. I am %s years old.", k->name, age_msg);
 }
